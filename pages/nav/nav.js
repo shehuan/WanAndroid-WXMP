8cbc688b7adc2546eb6775ae57a5bca09a505980
shehuan <shehuan@eshangm.com>
2019-11-29 18:17:28 +0800

    导航页面初始化

--- pages/nav/nav.js
+++ pages/nav/nav.js
@@ -1,66 +1,78 @@
-// pages/nav/nav.js
+let api = require('../../utils/api.js')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    selectIndex: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    api.navi()
+      .then(data => {
+        this.setData({
+          naviList: data
+        })
+      })
+  },
 
+  leftClick: function(event) {
+    this.setData({
+      selectIndex: event.currentTarget.dataset.index
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
