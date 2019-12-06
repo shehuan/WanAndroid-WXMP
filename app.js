efe5c9a3646f150d29d8d90253908d4da46be8cc
shehuan <shehuan@eshangm.com>
2019-12-06 16:42:59 +0800

    检测更新

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 App({
   onLaunch: function() {
     this.getNaviInfo();
+	this.checkUpdate();
 
     Array.prototype.remove = function(from, to) {
       var rest = this.slice((to || from) + 1 || this.length);
@@ -26,6 +27,39 @@ App({
         naviInfo.statusBarHeight = res.statusBarHeight;
       },
     })
+  },
+  
+  // 检查更新
+  checkUpdate: function () {
+    if (wx.canIUse('getUpdateManager')) {
+      let updateManager = wx.getUpdateManager()
+      updateManager.onCheckForUpdate(function (res) {
+        if (res.hasUpdate) {
+          updateManager.onUpdateReady(function () {
+            wx.showModal({
+              title: '更新提示',
+              content: '新版本已经准备好，是否重启应用？',
+              success: function (res) {
+                if (res.confirm) {
+                  updateManager.applyUpdate()
+                }
+              }
+            })
+          })
+          updateManager.onUpdateFailed(function () {
+            wx.showModal({
+              title: '已经有新版本了',
+              content: '新版本已经上线，请您删除当前小程序，重新搜索打开'
+            })
+          })
+        }
+      })
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+      })
+    }
   },
 
   globalData: {
